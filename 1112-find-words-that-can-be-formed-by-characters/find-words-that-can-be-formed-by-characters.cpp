0d02009e7f918f0befffad0121237f452c08068c
SHRIYANSH NEGI <162492217+SHRIYANSH-02113-NEGI@users.noreply.github.com>
2026-01-27 22:44:02 +0530

    Time: 7 ms (75.41%) | Memory: 24.1 MB (63.08%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -2,20 +2,15 @@ class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
         vector<int> charCount(26, 0);
-        
         for(char &ch : chars) {
             charCount[ch-'a']++;
         }
-        
         int result = 0;
-        
         for(string &word : words) {
-            
             vector<int> wordCount(26, 0);
             for(char &ch : word) {
                 wordCount[ch-'a']++;
             }
-            
             bool ok = true;
             for(int i = 0; i < 26; i++) {
                 if(wordCount[i] > charCount[i]) {
@@ -23,12 +18,9 @@ public:
                     break;
                 }
             }
-            
             if(ok)
                 result += word.size();
-            
         }
-        
         return result;
         
         
