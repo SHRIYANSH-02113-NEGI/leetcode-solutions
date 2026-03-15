92b3e6711a01c7fc441839a238a7bcf2f77cb723
SHRIYANSH NEGI <162492217+SHRIYANSH-02113-NEGI@users.noreply.github.com>
2026-03-15 15:30:58 +0530

    Time: 3 ms (64.08%) | Memory: 15.2 MB (5.76%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def singleNumber(self, nums):
-        arr = sorted(nums)
-        
-        if len(arr) == 1:
-            return arr[0]
+        freq = {}
 
-        for i in range(1, len(arr)-1):
-            if arr[i] != arr[i+1] and arr[i] != arr[i-1]:
-                return arr[i]
+        for num in nums:
+            if num not in freq:
+                freq[num] = 1
+            else:
+                freq[num] = freq[num] + 1
 
-        if arr[0] != arr[1]:
-            return arr[0]
-
-        return arr[-1]
+        for key, value in freq.items():
+            if value == 1:
+                return key
