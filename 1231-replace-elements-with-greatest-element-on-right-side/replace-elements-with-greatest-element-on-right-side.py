fa5e4557b32debcdd425c98a1441227f1d7bacce
SHRIYANSH NEGI <162492217+SHRIYANSH-02113-NEGI@users.noreply.github.com>
2026-03-16 19:22:15 +0530

    Time: 49 ms (38.07%) | Memory: 13.9 MB (67.74%) - LeetSync

--- 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/replace-elements-with-greatest-element-on-right-side.py
@@ -1,13 +1,8 @@
 class Solution(object):
     def replaceElements(self, arr):
-        largesttillnow = arr[-1]
-        n = len(arr)
-        if n == 1:
-            return [-1]
-        arr[n-1] = -1
-        for i in range(n-2, -1, -1):
-            temp = arr[i]
-            arr[i] = largesttillnow
-            largesttillnow = max(largesttillnow, temp)
-
+        max_right = -1
+        for i in range(len(arr)-1, -1, -1):
+            newMax = max(max_right, arr[i])
+            arr[i] = max_right
+            max_right = newMax    
         return arr
