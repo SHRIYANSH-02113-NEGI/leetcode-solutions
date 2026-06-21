c2cf4a75dbff92de3e73006984c36e75d8d98fc7
SHRIYANSH NEGI <162492217+SHRIYANSH-02113-NEGI@users.noreply.github.com>
2026-06-21 20:31:53 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (28.93%) - LeetSync

---
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> common;
+        unordered_set<int> s(nums1.begin(),nums1.end());
+        for(int &a:nums2){
+            if(s.count(a)!=0){
+                common.insert(a);
+            }
+        }
+        return vector<int>(common.begin(),common.end());
+    }
+};
